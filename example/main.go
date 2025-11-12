4509587f7759e9691b7c69e52e02116ee2baa38d
Kingsley <agukingsley450@gmail.com>
2025-11-12 22:31:47 +0100

    feat(main): create an example server to test the limiter

--- example/main.go
+++ example/main.go
@@ -1,1 +1,51 @@
-package example
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/kings0x/rlimiter/engine"
+	"github.com/kings0x/rlimiter/middleware"
+	"github.com/kings0x/rlimiter/requestlimiter"
+	"github.com/redis/go-redis/v9"
+)
+
+func ExampleClient() *redis.Client {
+	url := "your redis url"
+	opts, err := redis.ParseURL(url)
+	if err != nil {
+		panic(err)
+	}
+
+	return redis.NewClient(opts)
+}
+
+func main() {
+
+	rdb := ExampleClient()
+
+	reqLimiter := requestlimiter.New(requestlimiter.Options{
+		Rate:     1,
+		Capacity: 5,
+		Name:     "tokenbucket-req",
+		Store:    &requestlimiter.RedisStore{Client: rdb},
+	})
+
+	e := engine.New(reqLimiter)
+
+	mw := middleware.New(e, nil)
+
+	mux := http.NewServeMux()
+
+	mux.Handle("/", mw(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "OK %s\n", time.Now().Format(time.RFC3339))
+	})))
+
+	log.Println("listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+
+}
+
+//http://localhost:8080
